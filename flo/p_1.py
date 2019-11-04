8a4efa214002dfa9fd2e80743eb12bd85e766f84
babu <flofam11@gmail.com>
2019-11-04 15:10:42 -0500

    problem 1: columns converted to numeric

---
+++ flo/p_1.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+df_orders = pd.read_csv('../data/orders.csv')
+
+df_orders['Profit']
+
+# ----- HELPERS ----- 
+
+def strToNumeric(val):
+    _,b = val.split('$')
+    b = b.replace(',','')
+    return float(b)
+
+
+
+
+# ---- MAIN ------
+# column PROFIT to numeric
+df_orders[['Profit']] = df_orders[['Profit']].applymap(strToNumeric)
+
+
+# column SALE to numerif
+df_orders[['Sales']] = df_orders[['Sales']].applymap(strToNumeric)
